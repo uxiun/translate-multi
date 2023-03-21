c72436ee1d1078d55a0a59860d2c2e024c391d5c
uxiun <teppeiito0905@gmail.com>
2023-03-21 11:13:31 +0900

    workflow upd

--- next.config.js
+++ next.config.js
@@ -1,17 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : undefined // if "" instead of undefined: error
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
 }
 
-module.exports = {
-  ...nextConfig,
-  webpack: config => {
-    config.resolve.fallback = {fs: false}
-    return config
-  },
-}
+module.exports = nextConfig
+
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+// }
+
+// module.exports = {
+//   ...nextConfig,
+//   webpack: config => {
+//     config.resolve.fallback = {fs: false}
+//     return config
+//   },
+// }
 // {
 //   ...nextConfig,
 //   webpack: (config, { isServer }) => {
